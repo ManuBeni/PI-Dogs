f47c10e9dae4311ed34e048a57c54fb746856627
ManuBeni <manubeni0498@gmail.com>
2022-05-28 15:49:33 -0300

    Feat: Set-up para comenzar

--- api/index.js
+++ api/index.js
@@ -20,7 +20,8 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-// Syncing all the models at once.
+// Syncing all the models at once. 
+// CAMBIAR FORCE A TRUE PARA QUE NO BORRE LOS DATOS CUANDO SE SUBEN A LA DB
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
